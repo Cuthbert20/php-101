cff5b45bc69dcc8beb4402a25c6d7c44c223bebb
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-10 15:26:12 -0600

    set up form requirements

--- website3/index.php
+++ website3/index.php
@@ -1,3 +1,26 @@
+<?php
+    // Message Vars
+    $msg = '';
+    $msgClass = '';
+    // CHECK FOR SUBMIT looking for name="submit" on our button in the form
+    if(filter_has_var(INPUT_POST, 'submit')){
+        // GET FORM DATA and store in variables
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        // echo "{$name} {$email} {$message}";
+
+        // ALL FIELDS ARE REQUIRED, CHECKING REQUIRED FIELDS
+        if(!empty($name) && !empty($email) && !empty($message)){
+            // Passed
+            echo "PASSED";
+        }else{
+            // Failed
+            $msg = "Please fill in all fields";
+            $msgClass = "alert-danger";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +38,11 @@
         </div>
     </nav>
     <div class="container">
-        <form method='POST' action="">
+    <?php if($msg !== ''): ?>
+        <div class="alert <?php echo $msgClass; ?>" style=""><?php echo $msg; ?></div>
+    <?php endif; ?>
+        <!-- for our action on the form we could just use index.php but, instead we are using $_SERVER SUPER GLOBAL VARIABLE  -->
+        <form method='POST' action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
                 <label for="">Name</label>
                 <input type="text" name="name" class="form-control">
@@ -26,10 +53,10 @@
             </div>
             <div class="form-group">
                 <label for="">Message</label>
-                <input type="text" name="message" class="form-control">
+                <textarea name="message" class="form-control" cols="30" rows="10"></textarea>
             </div>
             <br>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button name="submit" type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
 </body>
