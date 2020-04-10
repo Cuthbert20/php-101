6e1394f199436eef9ee3ddb217b7c1538b2db0de
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-10 16:44:53 -0600

    set up conditional for email

--- website3/index.php
+++ website3/index.php
@@ -5,9 +5,9 @@
     // CHECK FOR SUBMIT looking for name="submit" on our button in the form
     if(filter_has_var(INPUT_POST, 'submit')){
         // GET FORM DATA and store in variables
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
         // echo "{$name} {$email} {$message}";
 
         // ALL FIELDS ARE REQUIRED, CHECKING REQUIRED FIELDS
@@ -16,11 +16,36 @@
             // CHECK EMAIL
             if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
                 // Failed
-                $msg = "Email Format Incorrect";
+                $msg = "Please enter valid email format";
                 $msgClass = "alert-danger";
             }else {
                 // Passed
-                echo "PASSED";
+            // RECIPIENT EMAIL
+            $toEmail = 'spencer.carey407@gmail.com';
+            $subject = "Contact Request From".$name;
+            $body = "<h2>Contact Request</h2>
+                <h4>Name:</h4><p>".$name.'</p>
+                <h4>Email:</h4><p>'.$email.'</p>
+                <h4>Message:</h4><p>'.$message."</p>";
+
+            // EMAIL HEADERS
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            # .= will append $headers variable not ovewrite it.
+            $headers .= "Content-Type:text/html;charset=UTF-8" . "\r\n";
+
+            // ADDITIONAL HEADERS
+            $headers .= "From: " .$name. "<".$email.">". "\r\n";
+
+            //IF THE EMAIL SENDS OR IT DOESN'T
+            if(mail($toEmail, $subject, $body, $headers)){
+                // EMAIL SENT
+                $msg = "Your email has been sent";
+                $msgClass = "alert-success";
+            } else {
+                // EMAIL FAILED TO SEND
+                $msg = "Your email was not sent";
+                $msgClass = "alert-danger";
+            }
             }
         }else{
             // Failed
@@ -53,15 +78,15 @@
         <form method='POST' action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="form-group">
                 <label for="">Name</label>
-                <input type="text" name="name" class="form-control">
+                <input value="<?php echo isset($_POST["name"]) ? $name: "";  ?>" type="text" name="name" class="form-control">
             </div>
             <div class="form-group">
                 <label for="">Email</label>
-                <input type="text" name="email" class="form-control">
+                <input value="<?php echo isset($_POST['email']) ? $email : ""; ?>" type="text" name="email" class="form-control">
             </div>
             <div class="form-group">
                 <label for="">Message</label>
-                <textarea name="message" class="form-control" cols="30" rows="10"></textarea>
+                <textarea  name="message" class="form-control" cols="30" rows="10"><?php echo isset($_POST["message"]) ? $message : ""; ?></textarea>
             </div>
             <br>
             <button name="submit" type="submit" class="btn btn-primary">Submit</button>
