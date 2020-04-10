a59274a9817ee8a9dfb3e0c3013f0d2a985a5639
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-10 12:39:22 -0600

    set up simple form for POST requests

---
+++ website3/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+    <title>Contact Us</title>
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">My Website</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <form method='POST' action="">
+            <div class="form-group">
+                <label for="">Name</label>
+                <input type="text" name="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="text" name="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Message</label>
+                <input type="text" name="message" class="form-control">
+            </div>
+            <br>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
