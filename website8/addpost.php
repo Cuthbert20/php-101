d9661dd19290fa8a4e3969465dcb21ffd1d7f20c
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-15 16:48:34 -0600

    set up query to get data from form and insert it into MySQL Table posts

--- website8/addpost.php
+++ website8/addpost.php
@@ -1,6 +1,25 @@
 <?php
     require "config/config.php";
     require "config/db.php";
+
+    // Check for submit
+    if(isset($_POST['submit'])){
+//        echo 'form has been submitted';
+        // Get form data, remember $conn short for connection comes from db.php file in config folder, where we are connecting to MySQL DB.
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $author = mysqli_real_escape_string($conn, $_POST['author']);
+        $body = mysqli_real_escape_string($conn, $_POST['body']);
+
+            // Creating our MySQL Query inserting post into posts Table. Make sure to use "" around the query string and '' around values as below.
+        $query = "INSERT INTO posts(title, author, body) VALUES ('$title', '$author', '$body')";
+
+        if(mysqli_query($conn, $query)){
+            // If successful redirect using header();
+            header('Location: ' . ROOT_URL . '');
+        }else{
+            echo 'ERROR: ' . mysqli_error($conn);
+        }
+    }
 ?>
 
 <?php include("inc/header.php"); ?>
@@ -13,7 +32,7 @@
         </div>
         <div class="form-group">
             <label>Author</label>
-            <input name="name" type="text" class="form-control">
+            <input name="author" type="text" class="form-control">
         </div>
         <div class="form-group">
             <label>Body</label>
