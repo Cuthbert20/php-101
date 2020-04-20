4515f6b804cc42f227dbc2e45409a5a0548576d0
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-19 19:13:18 -0600

    using multipule interfaces on classes and working with stric_types

--- oopsandbox/Interfaces/index.php
+++ oopsandbox/Interfaces/index.php
@@ -2,34 +2,70 @@
 declare(strict_types = 1);
 # Interfaces are blueprints that classes follow when using the keyword
 # Interfaces also help with type declaration, below I don't know what class will be passed into the pay method on BuyProduct class so...
-# I am using the interface PaymentMethod so that it knows which instance/obj of a class that I will be passing in references the interface PaymentMethod.
+# Using PaymentInterface any class that implements this interface will have to follow the rules set by the interface ie. must have a public function call payNow()
 // Interfaces
 interface PaymentInterface{
     # what rules do that classes that want to use this interface, need to follow? They need to have a method called payNow();
     public function payNow();
+    // interface can have several rules
+    public function paymentProcess();
 }
-    # Now the classes below that implement PaymentInterface are following the PaymentInterface interfaces rules.
-class Paypal implements PaymentInterface {
+
+# Second Interface, ie another set of rules.
+interface LoginInterface{
+    public function loginFirst();
+}
+
+    # Now the classes below follow rules that are implemented by the interface that the class implements;
+class Paypal implements PaymentInterface, LoginInterface {
+    // with paypal you would need to loginFirst() before paying unlike Cash or Visa.
+    public function loginFirst(){}
     public function payNow(){
+        echo 'Logged in and paid with paypal';
+    }
+    public function paymentProcess(){
+        $this->loginFirst();
+        $this->payNow();
+    }
+}
+
+class BankTransfer implements PaymentInterface, LoginInterface{
+    public function loginFirst(){
+        // TODO: Implement loginFirst() method.
+    }
+    public function payNow(){
+        echo 'Paid using bank transfer';
+    }
+    public function paymentProcess(){
+        $this->loginFirst();
+        $this->payNow();
     }
 }
 
 class Visa implements PaymentInterface {
-    public function payNow(){}
+    public function payNow(){
+        echo 'paid with Visa';
+    }
+    public function paymentProcess(){
+        $this->payNow();
+    }
 }
 
 class Cash implements  PaymentInterface {
     public function payNow(){
-        echo 'paid';
+        echo 'paid with cash';
+    }
+    public function paymentProcess(){
+        $this->payNow();
     }
 }
 
 class BuyProduct{
     public function pay(PaymentInterface $paymentType){
-    $paymentType->payNow();
+    $paymentType->paymentProcess();
     }
 }
 
-$paymentType = new Cash();
+$paymentType = new BankTransfer();
 $buyProduct = new BuyProduct();
 $buyProduct->pay($paymentType);
