2e2781fecb710d0ba12dbc26dee3574bfa2b34af
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-20 13:06:42 -0600

    setup Dbh class for connection to database, also created autoloader for classes

---
+++ oopsandbox/oop_php/classes/dbh.class.php
@@ -0,0 +1,20 @@
+<?php
+    require '../config.php';
+class Dbh {
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pwd = DB_PASS;
+    private $dbName = DB_NAME;
+
+    protected function connect(){
+        # Setting DSN param ie Data Source Name, describes exactly what type of database we're going to be using, what host we will be using and the name of the database we will be using.
+        R$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+        # Creating a PDO Connection.
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        # we are setting our connection so that our query's return Associative Arrays.
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pod;
+    }
+}
+echo ROOT_URL;
+
---
+++ oopsandbox/oop_php/index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'includes/class-autoload.inc.php';
+    ////////////////// In this section we are going to connect to a database using OOP PHP. We will focus on how to interact with databases using PDO //////////////
+    // See Link about PDO vs mysqli https://websitebeaver.com/php-pdo-vs-mysqli
+    ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Databases with OOP PHP</title>
+</head>
+<body>
+
+</body>
+</html>
