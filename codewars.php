37318fc743929e89a79611d577882fe63199a600
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-24 16:37:19 -0600

    some practice with different built-in string functions

--- codewars.php
+++ codewars.php
@@ -65,4 +65,51 @@ class Value{
 //$value1 = new Value("8 3 -5 42 -1 0 0 -9 4 7 4 -4");
 //echo $value1->highLow();
 
+// # Practice using built-in PHP functions
+$str = "The Wheels on the bus go round and round, round and round, round and round.";
+//    echo strlen($str);
 
+class Wars{
+    private $num;
+    private $str;
+
+    public function __construct($num, $str)
+    {
+        $this->num = $num;
+        $this->str = $str;
+    }
+// Take in a num and a string repeat the string $n amount of times.
+    public function strRepeat(){
+        return str_repeat($this->str, $this->num);
+    }
+
+    // Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer.
+    public function largestFiveDigits()
+    {
+        echo " need to refactor";
+    }
+    public function vowelCount(){
+        $this->str = strtolower($this->str);
+        return substr_count($this->str, 'a')+ substr_count($this->str, "e")+substr_count($this->str, "i")+substr_count($this->str, "o")+substr_count($this->str, "u");
+    }
+}
+
+$code = new Wars(3,"AbcdefoiuA");
+//echo $code->vowelCount();
+
+function xsOs(string $str){
+    $str = strtolower($str);
+    $x = 0;
+    $o = 0;
+    $arr = str_split($str);
+    foreach($arr as $val){
+        if($val === "o"){
+            $o++;;
+        }
+        if($val === "x"){
+            $x++;
+        }
+    }
+    return $x === $o;
+}
+echo xsOs("xoxoooabcxx");
